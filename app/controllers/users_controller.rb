e0741b7493212efe53f077fa07083b9615188798
miya-38 <miyacharge3803@gmail.com>
2026-02-14 03:00:17 +0000

    [Finish] 4-6

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  # 認証をスキップ: サインアップ（new, create）はログイン前に行うため
+  allow_unauthenticated_access only: [:new, :create] 
+ 
+  def new
+    @user = User.new
+  end
+ 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # ユーザー登録成功後、ログイン画面へリダイレクト
+      redirect_to new_session_path, notice: "ユーザー登録が完了しました！続けてログインしてください。"
+    else
+      # エラー時はフォームを再表示
+      render :new, status: :unprocessable_entity
+    end
+  end
+ 
+  private
+ 
+  def user_params
+    # name, email_address, password, password_confirmation を許可
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # ユーザー登録（サインアップ）のためのルーティングを追加
+  resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
+  
   resource :session
   resources :passwords, param: :token
   root to: "homes#top"
