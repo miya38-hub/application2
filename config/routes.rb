76bf3c736f2a7fd4de225fe6ec92a67f39e80bdc
miya38-hub <miyacharge3803@gmail.com>
2026-02-15 05:26:48 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # ユーザー登録（サインアップ）のためのルーティングを追加
-  resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
+  resources :users, only: [:new, :create, :show, :edit] , path_names: { new: 'sign_up' }
   
   get "homes/about", to: "homes#about", as: "about"
   resource :session
