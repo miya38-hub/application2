2d96f3641558ac71072451ea3308751d7a7f37d1
miya38-hub <miyacharge3803@gmail.com>
2026-02-15 06:15:26 +0000

    [Finish] 4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,22 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
- 
 
   def show
     @user = User.find(params[:id])  
     @post_images = @user.post_images    
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)    
+  end
+
   private
  
   def user_params
